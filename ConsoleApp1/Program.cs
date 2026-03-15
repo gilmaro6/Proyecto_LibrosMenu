911ea07a1805a1c5bdad446a16687110868983ae
felipe jimenez sanchez <felipe>
2026-03-15 13:19:43 -0500

    menu principal

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Proyecto estable");
+        bool salir = false;
+        while (!salir)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Menú Principal ===");
+            Console.WriteLine("1. Libros");
+            Console.WriteLine("2. Usuarios");
+            Console.WriteLine("3. Préstamos");
+            Console.WriteLine("4. Búsquedas y reportes");
+            Console.WriteLine("5. Guardar / Cargar datos");
+            Console.WriteLine("6. Salir");
+            Console.Write("Seleccione una opción: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                case "2": 
+                case "3":
+                case "4": 
+                case "5": 
+                case "6":
+                    Console.Write("saliendo del programa...");
+                    salir = true;
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida. Presione una tecla...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+
     }
 }
